38bd42c7514d00ee6f3ef8f5e91e0e73f06e3a90
Haboc, Bryan Joe <bryanjoehaboc241@gmail.com>
2022-02-06 16:38:57 +0800

    weather data and values fixed

--- main.js
+++ main.js
@@ -29,34 +29,87 @@ class ConvertLocToGeoCoor {
 }
 
 class Weather {
-  constructor(location) {
-    this.location = location;
+  constructor({ latitude, longitude }) {
+    this.latitude = latitude;
+    this.longitude = longitude;
+    this._apiKey = "48964d428f15677ab81748401048bf1c";
   }
 
-  getWeatherDataUsingLoc() {
-    console.log(location);
-    const apiKey = "48964d428f15677ab81748401048bf1c";
-    let weatherData = {};
-
-    fetch(
-      `https://api.openweathermap.org/data/2.5/onecall?q=${this.location}&exclude=current,hourly,minutely,alerts&units=metric&appid=48964d428f15677ab81748401048bf1c`,
+  async _getWeatherDataUsingLoc() {
+    return fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${this.latitude}&lon=${this.longitude}&exclude=current,hourly,minutely,alerts&units=metric&appid=${this._apiKey}`,
       {
         mode: "cors",
       }
-    ).then((res) => {
-      return res.json();
-    });
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        return res;
+      });
+  }
+
+  async getWeatherForecast() {
+    const obj = await this._getWeatherDataUsingLoc();
+    const arr = [];
+    for (let i = 0; i < obj.daily.length; i++) {
+      const tempObj = {
+        feelsLike: obj.daily[i].feels_like,
+        humidity: obj.daily[i].humidity,
+        temp: obj.daily[i].temp,
+        weather: obj.daily[i].weather,
+        windSpeed: obj.daily[i].wind_speed,
+      };
+      arr.push(tempObj);
+    }
+    return arr;
   }
 }
 
-class WeatherDisplay {
-  static;
+class TimeMethods {
+  constructor() {
+    this.dateInstance = new Date();
+  }
+  getTimeOfTheDay() {
+    //Morning is from sunrise to 11:59 AM. Sunrise typically occurs around 6 AM.
+    //Noon is at 12:00 PM.
+    //Afternoon is from 12:01 PM to around 6:00 PM.
+    //Evening is from 6:01 PM to 9 PM, or around sunset.
+    //Night is from sunset to sunrise, so from 9:01 PM until 5:59 AM.
+
+    const hourMin = parseIt(
+      this.dateInstance.getHours().toString() +
+        this.dateInstance.getMinutes().toString()
+    );
+
+    //
+    if (hourMin >= 0 && hourMin <= 659) return "eve";
+    else if (hourMin >= 700 && hourMin <= 1159) return "morn";
+    else if (hourMin >= 1200 && hourMin <= 1659) return "day";
+    else if (hourMin >= 1700 && 2359) return "night";
+  }
+
+  getDayOfWeek() {
+    const dayArr = [
+      "Sunday",
+      "Monday",
+      "Tueday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    return dayArr[this.dateInstance.getDay()];
+  }
 }
+
+class WeatherDisplay {}
 
 async function main() {
   const latlong = new ConvertLocToGeoCoor("Manila");
-  const wat = await latlong.getCoordinates();
-  const we = new Weather("Manila");
+  const geoData = await latlong.getCoordinates();
+  const weeatherData = new Weather(geoData);
 }
 
 main();
