607c63e53d2593f7dd2dc5981f4de840bb5a9a3e
Haboc, Bryan Joe <bryanjoehaboc241@gmail.com>
2022-02-06 22:14:45 +0800

    Left part remaining, find new background

--- main.js
+++ main.js
@@ -69,7 +69,17 @@ class Weather {
 
 class TimeMethods {
   constructor() {
-    this.dateInstance = new Date();
+    this._dateInstance = new Date();
+    this._dayArr = [
+      "Sunday",
+      "Monday",
+      "Tueday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    this._dayIndex = this._dateInstance.getDay();
   }
   getTimeOfTheDay() {
     //Morning is from sunrise to 11:59 AM. Sunrise typically occurs around 6 AM.
@@ -78,9 +88,9 @@ class TimeMethods {
     //Evening is from 6:01 PM to 9 PM, or around sunset.
     //Night is from sunset to sunrise, so from 9:01 PM until 5:59 AM.
 
-    const hourMin = parseIt(
-      this.dateInstance.getHours().toString() +
-        this.dateInstance.getMinutes().toString()
+    const hourMin = parseInt(
+      this._dateInstance.getHours().toString() +
+        this._dateInstance.getMinutes().toString()
     );
 
     //
@@ -90,26 +100,97 @@ class TimeMethods {
     else if (hourMin >= 1700 && 2359) return "night";
   }
 
-  getDayOfWeek() {
-    const dayArr = [
-      "Sunday",
-      "Monday",
-      "Tueday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-    return dayArr[this.dateInstance.getDay()];
+  getDayToday() {
+    return this._dayArr[this._dayIndex];
+  }
+
+  getNextDay() {
+    if (this._dayIndex === 6) this._dayIndex = 0;
+    else this._dayIndex++;
+
+    return this._dayArr[this._dayIndex];
   }
 }
 
-class WeatherDisplay {}
+// class Giphy {
+//   constructor() {
+//     this._apiKey = QbzkK6pGGcAEfeFjhO6h3XhJFFFh18Hm;
+//   }
 
-async function main() {
-  const latlong = new ConvertLocToGeoCoor("Manila");
-  const geoData = await latlong.getCoordinates();
-  const weeatherData = new Weather(geoData);
+// }
+class WeatherDisplay {
+  constructor(weatherData) {
+    this.weatherData = weatherData;
+    this.timeInstance = new TimeMethods();
+  }
+  //NOTE: cant do showLeftPart because of laptop lagging
+  showLeftPart() {}
+
+  showRightPart() {
+    //feels like
+    // humidity
+    //chance of rain
+    // wind speed
+    const feelsLikeDisplay = document.querySelector("#feels-like-info");
+    feelsLikeDisplay.innerHTML =
+      this.weatherData[0].feelsLike[
+        this.timeInstance.getTimeOfTheDay()
+      ].toFixed(2) + "\xB0";
+
+    const humidityDisplay = document.querySelector("#humidity-info");
+    humidityDisplay.innerHTML = this.weatherData[0].humidity + "%";
+
+    const windSpeedDisplay = document.querySelector("#wind-speed-info");
+    windSpeedDisplay.innerHTML = this.weatherData[0].windSpeed + " km/hr";
+  }
+
+  showCards() {
+    // day
+    // actual temp
+    // feels like
+    // logo
+
+    //TODO: delete createElement, hard code it in index.html, and just input values to innerHTML
+    for (let i = 1; i < this.weatherData.length; i++) {
+      const tod = this.timeInstance.getTimeOfTheDay();
+
+      const display = document.getElementById("card-" + i);
+
+      const dayDisplay = document.createElement("p");
+      dayDisplay.innerHTML = this.timeInstance.getNextDay();
+
+      const tempDisplay = document.createElement("p");
+      tempDisplay.innerHTML = this.weatherData[i].temp[tod].toFixed(2) + "\xB0";
+      tempDisplay.style.fontSize = "2em";
+
+      // const feelsLikeDisplay = document.createElement("p");
+      // feelsLikeDisplay.innerHTML = this.weatherData[i].feelsLike[tod];
+      // feelsLikeDisplay.style.fontSize = "1.25em";
+
+      const logo = document.createElement("img");
+
+      console.log(this.weatherData[i].weather.icon);
+
+      logo.setAttribute(
+        "src",
+        `http://openweathermap.org/img/w/${this.weatherData[i].weather[0].icon}.png`
+      );
+
+      display.append(dayDisplay);
+      display.append(tempDisplay);
+      display.append(logo);
+    }
+  }
 }
 
-main();
+(async function main() {
+  const latlong = new ConvertLocToGeoCoor("Manila");
+  const geoData = await latlong.getCoordinates();
+  const weatherData = new Weather(geoData);
+
+  const weatherDisplay = new WeatherDisplay(
+    await weatherData.getWeatherForecast()
+  );
+  weatherDisplay.showCards();
+  weatherDisplay.showRightPart();
+})();
